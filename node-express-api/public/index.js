0d5f0f0da7f45b5889525e50183f4815660e8066
brofus1 <sofusba@gmail.com>
2022-08-31 13:40:58 +0200

    lektie done

--- node-express-api/public/index.js
+++ node-express-api/public/index.js
@@ -1,4 +1,41 @@
 console.log('vi er her')
-fetch('10.78.66.74/api/mariotitles')
-    .then(res=> res.json())
-        .then(json=>console.log(json))
+let hvaler = document.getElementById('hvaler')
+hvaler.style.top = '90vh'
+let submitH = document.getElementById('submitH')
+
+let dato = document.getElementById('dato')
+dato.style.top = '80vh'
+let submitD = document.getElementById('submitD')
+
+document.querySelector('#hvaler h2').addEventListener('click', ()=>{
+    if(hvaler.style.top == '90vh'){
+        hvaler.style.top = 0;
+    }
+    else{
+        hvaler.style.top = '90vh'
+    }
+})
+
+document.querySelector('#dato h2').addEventListener('click', ()=>{
+    if(dato.style.top == '80vh'){
+        dato.style.top = 0;
+    }
+    else{
+        dato.style.top = '80vh'
+    }
+})
+
+submitH.addEventListener('click', ()=>{
+    fetch('http://localhost:1806/api/hvaler')
+        .then(res => res.json())
+            .then(json => document.querySelector('#hvaler .json').innerHTML = JSON.stringify(json))
+})
+
+submitD.addEventListener('click', ()=>{
+    fetch('http://localhost:1806/api/dato')
+        .then(res => res.json())
+            .then(json => document.querySelector('#dato .json').innerHTML = JSON.stringify(json))
+})
+
+
+
